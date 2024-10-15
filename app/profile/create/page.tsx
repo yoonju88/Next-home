4239eb308d3feb3e86983031899443564c63d392
yoonju88 <yoonju.t@gmail.com>
2024-10-15 12:01:21 +0200

    redirect to home page from create profile page if the app detect user profile

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -2,8 +2,12 @@ import SubmitButtons from '@/components/form/Buttons'
 import FormContainer from '@/components/form/FormContainer'
 import FormInput from '@/components/form/FormInput'
 import { createProfileAction } from '@/utils/action'
+import {currentUser} from '@clerk/nextjs/server'
+import {redirect} from 'next/navigation'
 
-export default function CreateProfilePage() {
+export default async function CreateProfilePage() {
+    const user = await currentUser()
+    if(user?.privateMetadata?.hasProfile) { redirect('/') }
     return (
         <section>
             <h1 className='text-2xl font-semibold mb-8 capitalize'>New user</h1>
