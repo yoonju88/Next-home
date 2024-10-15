cb74013cfa3811676d735300b982b0f317d4cc85
yoonju88 <yoonju.t@gmail.com>
2024-10-15 16:06:59 +0200

    Added InputContainer

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,16 +1,24 @@
 import FormContainer from '@/components/form/FormContainer';
-import { updateProfileAction, fetchProfile } from '@/utils/action';
+import { updateProfileAction, fetchProfile, updateProfileImageAction } from '@/utils/action';
 import FormInput from '@/components/form/FormInput';
 import SubmitButton  from '@/components/form/Buttons';
+import ImageInputContainer from '@/components/form/ImageInputContainer';
 
 async function ProfilePage() {
   const profile = await fetchProfile()
 
   return (
     <section>
-      <h1 className='text-2xl font-semibold mb-8 capitalize'>User profile</h1>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>
+        User profile
+      </h1>
       <div className='border p-8 rounded-md'>
-        
+        <ImageInputContainer 
+          image={profile.profileImage}
+          name={profile.username}
+          action={updateProfileImageAction}
+          text='Update Profile Image'
+        />
         <FormContainer action={updateProfileAction}>
           <div className='grid md:grid-cols-2 gap-4 mt-4' >
             <FormInput 
