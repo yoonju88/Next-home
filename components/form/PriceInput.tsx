36cfc1ac1ef1bdffb30d0d21c72cdf24163dc6b8
yoonju88 <yoonju.t@gmail.com>
2024-10-16 16:44:51 +0200

    Create property - create rental page

---
+++ components/form/PriceInput.tsx
@@ -0,0 +1,28 @@
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+import { Prisma } from '@prisma/client';
+
+const name = 'price'
+type PriceInputProps = {
+    defaultValue?: number;
+}
+
+function PriceInput({ defaultValue }: PriceInputProps) {
+    return (
+        <div className='mb-2'>
+            <Label htmlFor='price' className='capitalize'>
+                Price ($)
+            </Label>
+            <Input
+                id={name}
+                type='number'
+                name={name}
+                min={0}
+                defaultValue={defaultValue || 100}
+                required
+            />
+        </div>
+    )
+}
+
+export default PriceInput
