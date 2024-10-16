c95d41961760667f0d2d2e50da428be63b2c9738
yoonju88 <yoonju.t@gmail.com>
2024-10-16 15:42:29 +0200

    create property Schema

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,11 +1,11 @@
 import * as z from 'zod'
 import { ZodSchema } from 'zod'
 
-export const profileSchema = z.object ({
+export const profileSchema = z.object({
     //firstName: z.string().max(5, {message: 'max length is 5'})
-    firstName:z.string().min(2, {message: 'First name must be at least 2 characters'}),
-    lastName:z.string().min(2, {message: 'Last name must be at least 2 characters'}), 
-    username:z.string().min(2, {message: 'User name must be at least 2 characters'}),
+    firstName: z.string().min(2, { message: 'First name must be at least 2 characters' }),
+    lastName: z.string().min(2, { message: 'Last name must be at least 2 characters' }),
+    username: z.string().min(2, { message: 'User name must be at least 2 characters' }),
 })
 
 //  Zod 라이브러리를 사용하여 데이터 유효성을 검사하는 함수 validateWithZodSchema를 정의
@@ -13,10 +13,10 @@ export const profileSchema = z.object ({
 // schema는 Zod 스키마를 나타내고, data는 검증할 데이터입니다. 데이터는 unknown 타입으로 지정되어 유연성을 제공합니다.
 // 반환 타입은 T, 즉 유효성 검사를 통과한 데이터의 타입입니다.
 
-export function validateWithZodSchema<T>(schema:ZodSchema<T>,data:unknown):T {
+export function validateWithZodSchema<T>(schema: ZodSchema<T>, data: unknown): T {
     const result = schema.safeParse(data) // safeParse 메서드는 주어진 데이터를 스키마에 따라 안전하게 검증
 
-    if(!result.success) {
+    if (!result.success) {
         const errors = result.error.errors.map((error) => error.message)
         throw new Error(errors.join(','))
     }
@@ -35,15 +35,61 @@ export function validateWithZodSchema<T>(schema:ZodSchema<T>,data:unknown):T {
 // 	•	이 함수는 다양한 Zod 스키마와 데이터에 대해 유효성 검사를 수행할 수 있도록 설계되었습니다. 따라서 코드 중복을 줄이고, 유효성 검사 로직을 중앙 집중화할 수 있습니다.
 
 export const imageSchema = z.object({
-    image:validateFile(),
+    image: validateFile(),
 })
 
 function validateFile() {
-    const maxUploadSize = 1024 *1024;
+    const maxUploadSize = 1024 * 1024;
     const acceptedFileTypes = ['image/']
-    return z.instanceof(File).refine((file)=> {
+    return z.instanceof(File).refine((file) => {
         return !file || file.size <= maxUploadSize
-    }, 'File size must be less than 1 MB').refine((file)=> {
+    }, 'File size must be less than 1 MB').refine((file) => {
         return !file || acceptedFileTypes.some((type) => file.type.startsWith(type))
     }, 'File must be an image')
 }
+
+export const propertySchema = z.object({
+    name: z
+        .string()
+        .min(2, {
+            message: 'name must be at least 2 characters.',
+        })
+        .max(100, {
+            message: 'name must be less than 100 characters.',
+        }),
+    tagline: z
+        .string()
+        .min(2, {
+            message: 'tagline must be at least 2 characters.',
+        })
+        .max(100, {
+            message: 'tagline must be less than 100 characters.',
+        }),
+    price: z.coerce.number().int().min(0, {
+        message: 'price must be a positive number.',
+    }),
+    category: z.string(),
+    description: z.string().refine(
+        (description) => {
+            const wordCount = description.split(' ').length;
+            return wordCount >= 10 && wordCount <= 1000;
+        },
+        {
+            message: 'description must be between 10 and 1000 words.',
+        }
+    ),
+    country: z.string(),
+    guests: z.coerce.number().int().min(0, {
+        message: 'guest amount must be a positive number.',
+    }),
+    bedrooms: z.coerce.number().int().min(0, {
+        message: 'bedrooms amount must be a positive number.',
+    }),
+    beds: z.coerce.number().int().min(0, {
+        message: 'beds amount must be a positive number.',
+    }),
+    baths: z.coerce.number().int().min(0, {
+        message: 'bahts amount must be a positive number.',
+    }),
+    amenities: z.string(),
+});
