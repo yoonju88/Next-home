f3df426431fb698360303625da3a54014567257f
yoonju88 <yoonju.t@gmail.com>
2024-10-17 09:58:46 +0200

    Added counter Input

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -8,6 +8,7 @@ import CategoriesInput from '@/components/form/CategoriesInput'
 import TextAreaInput from '@/components/form/TextAreaInput'
 import ImageInput from '@/components/form/ImageInput'
 import CountriesInput from '@/components/form/CountriesInput'
+import CounterInput from '@/components/form/CounterInput'
 
 function CreatePropertyPage() {
     return (
@@ -40,12 +41,15 @@ function CreatePropertyPage() {
                         <CountriesInput />
                         <ImageInput />
                     </div>
+                    <h3 className='text-lg mt-8 mb-4 font-medium'>Accommodation Details</h3>
+                    <CounterInput detail='guests' />
+                    <CounterInput detail='bedrooms' />
+                    <CounterInput detail='beds' />
+                    <CounterInput detail='baths' />
                     <SubmitButton text='create rental' className='mt-12' />
                 </FormContainer>
             </div>
-
         </section>
     )
 }
-
 export default CreatePropertyPage
--- components/form/CounterInput.tsx
+++ components/form/CounterInput.tsx
@@ -4,3 +4,57 @@ import { LuMinus, LuPlus } from 'react-icons/lu'
 import { Button } from '../ui/button'
 import { useState } from 'react'
 
+export default function CounterInput({
+    detail, defaultValue, }: { detail: string; defaultValue?: number; }) {
+    const [count, setCount] = useState(defaultValue || 0)
+    const increaseCount = () => {
+        setCount((prevCount) => {
+            if (prevCount < 10) {
+                return prevCount + 1
+            }
+            return prevCount
+        })
+    }
+
+    const decreaseCount = () => {
+        setCount((prevCount) => {
+            if (prevCount > 0) {
+                return prevCount - 1
+            }
+            return prevCount
+        })
+    }
+
+    return (
+        <Card className='mb-4'>
+            <input type='hidden' name={detail} value={count} /> {/*to provide the value to server */}
+            <CardHeader className='flex flex-col gapy-5'>
+                <div className='flex items-center justify-between flex-wrap'>
+                    <div className='flex flex-col'>
+                        <h2 className='font-medium capitalize'>{detail}</h2>
+                        <p className='text-muted-foreground text-sm'>Specify the number of {detail}</p>
+                    </div>
+                    <div className='flex items-center gap-4'>
+                        <Button
+                            variant='outline'
+                            size='icon'
+                            type='button'
+                            onClick={decreaseCount}
+                        >
+                            <LuMinus className='w-5 h-5 text-primary' />
+                        </Button>
+                        <span className='text-xl font-bold w-5 text-center'>{count}</span>
+                        <Button
+                            variant='outline'
+                            size='icon'
+                            type='button'
+                            onClick={increaseCount}
+                        >
+                            <LuPlus className='w-5 h-5 text-primary' />
+                        </Button>
+                    </div>
+                </div>
+            </CardHeader>
+        </Card>
+    )
+}
