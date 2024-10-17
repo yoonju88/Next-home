ae297148a64c57b730cf07caa05165853053e671
yoonju88 <yoonju.t@gmail.com>
2024-10-17 15:26:41 +0200

    Added CreateProperties action and fetch properties

--- utils/action.ts
+++ utils/action.ts
@@ -5,6 +5,7 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { imageSchema, profileSchema, propertySchema, validateWithZodSchema } from "./schemas"
 import { uploadImage } from './supabase';
+import { string } from 'zod';
 
 const getAuthUser = async () => {
     const user = await currentUser()
@@ -12,7 +13,6 @@ const getAuthUser = async () => {
         throw new Error('Please logged in to access this page')
     }
     if (!user.privateMetadata.hasProfile) redirect('/profile/create')
-    console.log(user)
     return user
 }
 
@@ -22,8 +22,6 @@ const renderError = (error: unknown): { message: string } => {
         { message: error instanceof Error ? error.message : 'An error occurred' }
     )
 }
-
-
 export const createProfileAction = async (
     prevState: any,
     formData: FormData
@@ -31,7 +29,6 @@ export const createProfileAction = async (
     try {
         const user = await currentUser()
         if (!user) throw new Error('Please login to create a profile')
-
         const rawData = Object.fromEntries(formData)
         const validatedFields = validateWithZodSchema(profileSchema, rawData)
         await db.profile.create({
@@ -69,6 +66,7 @@ export const fetchProfileImage = async () => {
 
 export const fetchProfile = async () => {
     const user = await getAuthUser();
+    console.log('userID:', user.id)
     const profile = await db.profile.findUnique({
         where: {
             clerkId: user.id,
@@ -131,10 +129,47 @@ export const createPropertyAction = async (
     const user = await getAuthUser()
     try {
         const rawData = Object.fromEntries(formData)
+        const file = formData.get('image') as File
         const validatedFields = validateWithZodSchema(propertySchema, rawData)
-        return { message: 'Property created' }
+        const validatedFile = validateWithZodSchema(imageSchema, { image: file })
+        const fullPath = await uploadImage(validatedFile.image)
+        await db.property.create({
+            data: {
+                ...validatedFields,
+                image: fullPath,
+                profileId: user.id,
+            },
+        })
     } catch (error) {
         return renderError(error)
     }
-    //redirect('/')
+    redirect('/')
+}
+
+// to search bar
+export const fetchProperties = async ({
+    search = '',
+    category,
+}: {
+    search?: string;
+    category?: string;
+}) => {
+    const properties = await db.property.findMany({
+        where: {
+            category,
+            OR: [
+                { name: { contains: search, mode: 'insensitive' } },
+                { tagline: { contains: search, mode: 'insensitive' } },
+            ],
+        },
+        select: {
+            id: true,
+            name: true,
+            tagline: true,
+            country: true,
+            image: true,
+            price: true,
+        },
+    })
+    return properties;
 }
