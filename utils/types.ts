06d2666bb82c0c7043ff092661cf538ba26f9d23
yoonju88 <yoonju.t@gmail.com>
2024-10-17 15:27:00 +0200

    Added PropertuCardProps

--- utils/types.ts
+++ utils/types.ts
@@ -1,4 +1,14 @@
-export type actionFunction =(
-    prevState:any, 
-    formData:FormData
+export type actionFunction = (
+    prevState: any,
+    formData: FormData
 ) => Promise<{ message: string }>
+
+
+export type PropertyCardProps = {
+    image: string;
+    id: string;
+    name: string;
+    tagline: string;
+    country: string;
+    price: number;
+};
