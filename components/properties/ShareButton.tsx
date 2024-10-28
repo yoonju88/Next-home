abd1f3427b8787b2df7ec40797c74663456e7f66
yoonju88 <yoonju.t@gmail.com>
2024-10-28 16:52:22 +0100

    fix issue of  React.Children.only expected to receive a single React element child

--- components/properties/ShareButton.tsx
+++ components/properties/ShareButton.tsx
@@ -28,30 +28,29 @@ function ShareButton({
 
     const url = process.env.NEXT_PUBLIC_WEBSITE_URL
     const shareLink = `${url}/properties/${propertyId}`
-    console.log('Website URL:', process.env.NEXT_PUBLIC_WEBSITE_URL)
     return (
         <Popover>
             <PopoverTrigger asChild>
                 <Button variant='outline' size='icon' className='p-2'>
                     <LuShare2 />
                 </Button>
-                <PopoverContent
-                    side='top'
-                    align='end'
-                    sideOffset={10}
-                    className='flex items-center gap-x-2 justify-center w-full'
-                >
-                    <TwitterShareButton url={shareLink} title={name}>
-                        <TwitterIcon size={32} round />
-                    </TwitterShareButton>
-                    <LinkedinShareButton url={shareLink} title={name}>
-                        <LinkedinIcon size={32} round />
-                    </LinkedinShareButton>
-                    <EmailShareButton url={shareLink} subject={name}>
-                        <EmailIcon size={32} round />
-                    </EmailShareButton>
-                </PopoverContent>
             </PopoverTrigger>
+            <PopoverContent
+                side='top'
+                align='end'
+                sideOffset={10}
+                className='flex items-center gap-x-2 justify-center w-full'
+            >
+                <TwitterShareButton url={shareLink} title={name}>
+                    <TwitterIcon size={32} round />
+                </TwitterShareButton>
+                <LinkedinShareButton url={shareLink} title={name}>
+                    <LinkedinIcon size={32} round />
+                </LinkedinShareButton>
+                <EmailShareButton url={shareLink} subject={name}>
+                    <EmailIcon size={32} round />
+                </EmailShareButton>
+            </PopoverContent>
         </Popover>
     )
 }
