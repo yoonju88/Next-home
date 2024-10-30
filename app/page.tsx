0aa2aaf0b3ec82a92cec3fcb001438ece702f56b
yoonju88 <yoonju.t@gmail.com>
2024-10-30 10:26:50 +0100

    add await to fetch data of category and search

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,26 @@ import CategoriesList from '@/components/home/CategoriesList'
 import PropertiesContainer from '@/components/home/PropertiesContainer'
 import { Suspense } from 'react'
 import LoadingCard from '@/components/card/LoadingCard'
-//import { categories } from '@/utils/categories'
 
 async function HomePage({
   searchParams,
 }: {
   searchParams: { category?: string; search?: string; }
 }) {
+
+  const { category } = await searchParams
+  const { search } = await searchParams
+
   return (
     <section>
       <CategoriesList
-        category={searchParams.category}
-        search={searchParams.search}
+        category={category}
+        search={search}
       />
       <Suspense fallback={<LoadingCard />}>
         <PropertiesContainer
-          category={searchParams.category}
-          search={searchParams.search}
+          category={category}
+          search={search}
         />
       </Suspense>
     </section>
