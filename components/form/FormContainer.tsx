4f42b9391168a520250746663cfbfb5a7b91a3d6
yoonju88 <yoonju.t@gmail.com>
2024-10-30 10:27:15 +0100

    change to useActionState

--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useFormState } from "react-dom"
+import { useActionState } from "react"
 import { useEffect } from "react"
 import { useToast } from '@/hooks/use-toast'
 import { actionFunction } from '@/utils/types'
@@ -17,7 +17,7 @@ export default function FormContainer({
     action: actionFunction,
     children: React.ReactNode
 }) {
-    const [state, formAction] = useFormState(action, initialState)
+    const [state, formAction] = useActionState(action, initialState)
     const { toast } = useToast()
 
     useEffect(() => {
