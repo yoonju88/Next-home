c22910fdaa13d245ee545a9f55c6b33be7fd16ec
yoonju88 <yoonju.t@gmail.com>
2024-10-30 12:08:49 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,13 @@ import CategoriesList from '@/components/home/CategoriesList'
 import PropertiesContainer from '@/components/home/PropertiesContainer'
 import { Suspense } from 'react'
 import LoadingCard from '@/components/card/LoadingCard'
-import { GetServerSideProps } from 'next';
-
-type SearchParams = {
-  category?: string;
-  search?: string;
-};
-
-type HomePageProps = {
-  searchParams: SearchParams;
-};
 
 function HomePage({
   searchParams,
-}: HomePageProps
-) {
-  const { category, search } = searchParams;
+}: {
+  searchParams: { category?: string; search?: string; }
+}) {
+  const { category, search } = searchParams
 
   return (
     <section>
@@ -35,18 +26,5 @@ function HomePage({
   )
 }
 export default HomePage
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { category, search } = context.query;
-
-  return {
-    props: {
-      searchParams: {
-        category: category || null,
-        search: search || null,
-      },
-    },
-  };
-};
 
 
