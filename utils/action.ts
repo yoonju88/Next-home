d3c68d02a74278468e5eeb2a54631651bf92b0d3
yoonju88 <yoonju.t@gmail.com>
2024-11-06 15:06:18 +0100

    Add reservations page

--- utils/action.ts
+++ utils/action.ts
@@ -586,3 +586,32 @@ export const updatePropertyImageAction = async (
         return renderError(error)
     }
 }
+
+
+//Fetch Reservations
+
+export const fetchReservations = async () => {
+    const user = await getAuthUser()
+
+    const reservations = await db.booking.findMany({
+        where: {
+            property: {
+                profileId: user.id,
+            },
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+        include: {
+            property: {
+                select: {
+                    id: true,
+                    name: true,
+                    price: true,
+                    country: true,
+                },
+            },
+        },
+    })
+    return reservations
+}
