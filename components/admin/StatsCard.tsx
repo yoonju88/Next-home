2fb0e029c2d34671b759f85a5d83fbe6ad563404
yoonju88 <yoonju.t@gmail.com>
2024-11-07 10:26:27 +0100

    create chart and stats card component

---
+++ components/admin/StatsCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Card, CardHeader } from '@/components/ui/card';
+
+type StatsCardProps = {
+    title: string;
+    value: number;
+}
+
+function StatsCard({ title, value }: StatsCardProps) {
+    return (
+        <Card className='bg-muted'>
+            <CardHeader className='flex flex-row justify-between items-center'>
+                <h3 className='capitalize text-3xl font-bold'>{title}</h3>
+                <span className='text-primary text-5xl font-extrabold'>{value}</span>
+            </CardHeader>
+        </Card>
+    )
+}
+export default StatsCard
