70e56fb13022719a63a42db68cb1ddb62bf85fbb
yoonju88 <yoonju.t@gmail.com>
2024-11-18 13:10:14 +0100

    ..

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -3,11 +3,11 @@
 import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
-import { type NextRequest, type NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
 import db from '@/utils/db'
 import { formatDate } from '@/utils/format'
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
+export const POST = async (req: NextRequest) => {
     // This extracts headers from req to get the header, which represents the base URL of the request's origin
     const requestHeaders = new Headers(req.headers)
     const origin = requestHeaders.get('origin')
