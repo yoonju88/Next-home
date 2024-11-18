886218a6e310afe83f137c5ad99c0c15b09ac958
yoonju88 <yoonju.t@gmail.com>
2024-11-18 12:57:49 +0100

    remove 'res' never used

--- app/api/confrim/route.ts
+++ app/api/confrim/route.ts
@@ -1,10 +1,10 @@
 import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 import { redirect } from 'next/navigation'
-import { type NextRequest, type NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
 import db from '@/utils/db'
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
+export const GET = async (req: NextRequest) => {
     const { searchParams } = new URL(req.url);
     const session_id = searchParams.get('session_id') as string
     try {
